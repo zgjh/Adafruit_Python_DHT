af722179a93a17a59dd2a762e94212e5f4e04aa8
zgjh <37134125+zgjh@users.noreply.github.com>
2018-11-22 15:44:55 +0800

    DHT

--- examples/AdafruitDHT1.py
+++ examples/AdafruitDHT1.py
@@ -30,7 +30,7 @@ sensor_args = { '11': Adafruit_DHT.DHT11,
                 '2302': Adafruit_DHT.AM2302 }
 if len(sys.argv) == 3 and sys.argv[1] in sensor_args:
     sensor = sensor_args[sys.argv[1]]
-    pin = sys.argv[2]
+    pin = sys.argv[2] 
 else:
     print('Usage: sudo ./Adafruit_DHT.py [11|22|2302] <GPIO pin number>')
     print('Example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO pin #4')
